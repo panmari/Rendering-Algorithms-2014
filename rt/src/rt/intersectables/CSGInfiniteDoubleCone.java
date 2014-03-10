89c901499c3c4cf2bbc1478541ebe650d115fb8e
panmari <smmuzi@gmail.com>
2014-03-10 14:09:52 +0100

    fixed normal of cone

--- rt/src/rt/intersectables/CSGInfiniteDoubleCone.java
+++ rt/src/rt/intersectables/CSGInfiniteDoubleCone.java
@@ -64,7 +64,7 @@ public class CSGInfiniteDoubleCone extends CSGSolid {
 		tangential1.cross(new Vector3f(0, 0, 1), normalCyl);
 		Vector3f tangential2 = new Vector3f(hitPoint);
 		Vector3f normal = new Vector3f();
-		normal.cross(tangential2, tangential1);
+		normal.cross(tangential1, tangential2);
 		normal.normalize();
 		
 		Vector3f wIn = new Vector3f(r.direction);
