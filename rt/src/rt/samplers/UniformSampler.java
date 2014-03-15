abfa24697b33debbbb7cb0d53137edea34410ff2
panmari <smmuzi@gmail.com>
2014-03-15 08:19:53 +0100

    implemented Uniform sampler
    ...not tested yet

--- rt/src/rt/samplers/UniformSampler.java
+++ rt/src/rt/samplers/UniformSampler.java
@@ -4,22 +4,19 @@ import rt.Sampler;
 
 public class UniformSampler implements Sampler {
 
-	public UniformSampler() {
-		throw new RuntimeException("not implemented yet");
-	}
-	
+	/**
+	 * Expects n to be a square number, d to be 2.
+	 * If not, n will be rounded up to the next square number
+	 */
 	@Override
 	public float[][] makeSamples(int n, int d) {
-		float dist = 1.f/n;
-		float samples[][] = new float[n][d];
-		for(int i=0; i<n; i++)
-		{
-			float sample = dist*(i+1);
-			
-			for(int j=0; j<d; j++)
-			{
-				samples[i][j] = sample + dist*(i + 1);
-			}
+		int k = (int) Math.ceil(Math.sqrt(n));
+		float dist = 1.f/k;
+		float offset = 1.f/(k*2);
+		float samples[][] = new float[k*k][d];
+		for (int i = 0; i < k*k; i++) {
+			samples[i][0] = dist + offset*i;
+			samples[i][1] = dist + offset*(i / k); //integer division here!
 		}
 
 		return samples;
