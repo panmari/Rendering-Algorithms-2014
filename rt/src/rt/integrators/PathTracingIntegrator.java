8b7a9da56daae8174d06ca30b1b4a9d686421c94
panmari <smmuzi@gmail.com>
2014-04-11 16:30:52 +0200

    first try at path tracing

---
+++ rt/src/rt/integrators/PathTracingIntegrator.java
@@ -0,0 +1,152 @@
+package rt.integrators;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import javax.vecmath.Vector3f;
+
+import rt.HitRecord;
+import rt.Integrator;
+import rt.Intersectable;
+import rt.LightGeometry;
+import rt.LightList;
+import rt.Material.ShadingSample;
+import rt.Ray;
+import rt.Sampler;
+import rt.Scene;
+import rt.Spectrum;
+import rt.integrators.heuristics.PowerHeuristic;
+import rt.samplers.RandomSampler;
+import util.StaticVecmath;
+
+public class PathTracingIntegrator implements Integrator {
+
+	final int nrBounces = 1;
+	//final float[] russianRouletteProbabilities = new float[]{0,0,.5f,.5f,.5f,.5f,.5f,.5f,.5f,1};
+	// two bounces
+	private LightList lightList;
+	private Intersectable root;
+	private RandomSampler sampler;
+	
+	public PathTracingIntegrator(Scene scene) {
+		this.lightList = scene.getLightList();
+		this.root = scene.getIntersectable();
+		this.sampler = new RandomSampler();
+	}
+	
+	@Override
+	public Spectrum integrate(Ray primaryRay) {
+		Ray currentRay = primaryRay;
+		Spectrum outgoing = new Spectrum();
+		Random a = new Random();
+		Spectrum alpha = new Spectrum(1);
+		RussianRouletteIterator rr = new RussianRouletteIterator(0f,0f,0f,1f);
+		for(int bounce = 0;; bounce++) {
+			HitRecord hit = root.intersect(currentRay);
+			if (hit == null)
+				break;
+			Spectrum emission = hit.material.evaluateEmission(hit, hit.w);
+			if (bounce == 0 && emission != null) {
+				outgoing.add(emission);
+				//break;
+			}
+			Spectrum x = sampleLight(hit);
+			Spectrum currentBounceContribution = new Spectrum(alpha);
+			currentBounceContribution.mult(x);
+			outgoing.add(currentBounceContribution);
+			Float rrProbability = rr.next();
+			if (a.nextFloat() < rrProbability)
+				break;
+			ShadingSample s = hit.material.getShadingSample(hit, this.sampler.makeSamples(1, 2)[0]);
+			if (s == null) // hit light -> break
+				break;
+			currentRay = new Ray(hit.position, s.w, bounce + 1, true);
+			alpha.mult(s.brdf);
+			alpha.mult(hit.normal.dot(s.w)/(s.p*(1-rrProbability)));
+		}
+		return outgoing;
+	}
+	
+	private Spectrum sampleLight(HitRecord hitRecord) {
+		float[][] sample = this.sampler.makeSamples(1, 2);
+		LightGeometry lightSource = lightList.getRandomLight(this.sampler.makeSamples(1, 2));
+
+		HitRecord lightHit = lightSource.sample(sample[0]);
+		
+		Vector3f lightDir = StaticVecmath.sub(lightHit.position, hitRecord.position);
+		float d2 = lightDir.lengthSquared();
+		lightDir.normalize();
+		
+		Ray shadowRay = new Ray(hitRecord.position, lightDir, 0, true);
+		HitRecord shadowHit = root.intersect(shadowRay);
+		if (shadowHit != null &&
+				StaticVecmath.dist2(shadowHit.position, hitRecord.position) + 1e-5f < d2) //only if closer than light
+			return new Spectrum();
+		
+		// Evaluate the BRDF, probability is saved in p of hitRecord
+		Spectrum brdfValue = hitRecord.material.evaluateBRDF(hitRecord, hitRecord.w, lightDir);		 
+		
+		Spectrum s = new Spectrum(brdfValue);
+		// Multiply with emission
+		Spectrum emission = lightHit.material.evaluateEmission(lightHit, StaticVecmath.negate(lightDir));
+		
+		// Multiply with cosine of surface normal and incident direction
+		float ndotl = hitRecord.normal.dot(lightDir);
+		ndotl = Math.max(ndotl, 0.f);
+		emission.mult(ndotl);
+		
+		s.mult(emission);
+		
+		float cosLight = Math.max(lightHit.normal.dot(StaticVecmath.negate(lightDir)), 0);
+		
+	
+		// adapt probability to hit exactly that light
+		float probability = lightHit.p/lightList.size();
+		
+		// some geometric terms
+		//s.mult(1.f/(d2*probability));
+		s.mult(cosLight);
+
+		// turn into directional probability
+		float dirProbablity = probability * d2 /cosLight;
+		s.mult(1f/dirProbablity);
+		return s;
+	}
+
+	@Override
+	public float[][] makePixelSamples(Sampler sampler, int n) {
+		return sampler.makeSamples(n, 2);
+	}
+
+	private class RussianRouletteIterator implements Iterator<Float>{
+
+		int i = 0;
+		private float[] floats;
+		private float last;
+		
+		public RussianRouletteIterator(float... floats) {
+			this.floats = floats;
+			this.last = floats[floats.length - 1];
+		}
+		@Override
+		public boolean hasNext() {
+			return true;
+		}
+
+		@Override
+		public Float next() {
+			if (i + 1 < floats.length) {
+				float next = floats[i];
+				i++;
+				return next;
+			} else
+				return last;
+		}
+
+		@Override
+		public void remove() {
+			throw new RuntimeException("Not supported.");
+		}
+		
+	}
+}
---
+++ rt/src/rt/integrators/PathTracingIntegratorFactory.java
@@ -0,0 +1,19 @@
+package rt.integrators;
+
+import rt.Integrator;
+import rt.IntegratorFactory;
+import rt.Scene;
+
+public class PathTracingIntegratorFactory implements IntegratorFactory {
+
+	@Override
+	public Integrator make(Scene scene) {
+		return new PathTracingIntegrator(scene);
+	}
+
+	@Override
+	public void prepareScene(Scene scene) {
+		// NOTHING
+	}
+
+}
