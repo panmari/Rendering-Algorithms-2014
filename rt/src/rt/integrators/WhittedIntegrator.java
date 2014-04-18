99780a3bf6af53a73d66c0bf974637e00ecbca42
panmari <smmuzi@gmail.com>
2014-04-18 15:38:14 +0200

    fixed whitted integrator for area lights

--- rt/src/rt/integrators/WhittedIntegrator.java
+++ rt/src/rt/integrators/WhittedIntegrator.java
@@ -11,6 +11,7 @@ import rt.Intersectable;
 import rt.LightGeometry;
 import rt.LightList;
 import rt.Material.ShadingSample;
+import rt.samplers.RandomSampler;
 import rt.Ray;
 import rt.Sampler;
 import rt.Scene;
@@ -22,11 +23,13 @@ public class WhittedIntegrator implements Integrator {
 	LightList lightList;
 	Intersectable root;
 	private final int MAX_DEPTH = 5;
+	private RandomSampler sampler;
 	
 	public WhittedIntegrator(Scene scene)
 	{
 		this.lightList = scene.getLightList();
 		this.root = scene.getIntersectable();
+		this.sampler = new RandomSampler();
 	}
 
 	@Override
@@ -35,6 +38,10 @@ public class WhittedIntegrator implements Integrator {
 		HitRecord hitRecord = root.intersect(r);
 		if (hitRecord == null)
 			return new Spectrum(0.f,0.f,0.f);
+		
+		Spectrum emission = hitRecord.material.evaluateEmission(hitRecord, hitRecord.w);
+		if (emission != null)
+			return emission;
 		
 		//follow specular refraction until maxdepth
 		Spectrum reflectedPart = new Spectrum(0,0,0);
@@ -74,8 +81,8 @@ public class WhittedIntegrator implements Integrator {
 		{
 			LightGeometry lightSource = it.next();
 			
-			// Make direction from hit point to light source position; this is only supposed to work with point lights
-			HitRecord lightHit = lightSource.sample(null);
+			float[][] sample = this.sampler.makeSamples(1, 2);
+			HitRecord lightHit = lightSource.sample(sample[0]);
 			Vector3f lightDir = StaticVecmath.sub(lightHit.position, hitRecord.position);
 			float d2 = lightDir.lengthSquared();
 			lightDir.normalize();
@@ -94,14 +101,19 @@ public class WhittedIntegrator implements Integrator {
 			// Multiply with emission
 			s.mult(lightHit.material.evaluateEmission(lightHit, StaticVecmath.negate(lightDir)));
 			
+			float cosLight = 1; //for point lights
+			if (lightHit.normal != null)
+				cosLight = Math.max(lightHit.normal.dot(StaticVecmath.negate(lightDir)), 0);
+			
 			// Multiply with cosine of surface normal and incident direction
 			float ndotl = hitRecord.normal.dot(lightDir);
 			ndotl = Math.max(ndotl, 0.f);
-			s.mult(ndotl);
+			s.mult(ndotl*cosLight);
 			
+
 			// Geometry term: multiply with 1/(squared distance), only correct like this 
 			// for point lights (not area lights)!
-			s.mult(1.f/d2);
+			s.mult(1.f/(d2*lightHit.p));
 			
 			// Accumulate
 			outgoing.add(s);
