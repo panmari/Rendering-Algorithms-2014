fbf73fbebd2c5996650c65dbe02cf6723cada88c
panmari <smmuzi@gmail.com>
2014-04-27 15:59:37 +0200

    rriterator class is now in own file

--- rt/src/rt/integrators/PathTracingIntegrator.java
+++ rt/src/rt/integrators/PathTracingIntegrator.java
@@ -1,6 +1,5 @@
 package rt.integrators;
 
-import java.util.Iterator;
 import java.util.Random;
 
 import javax.vecmath.Vector3f;
@@ -126,37 +125,5 @@ public class PathTracingIntegrator implements Integrator {
 	@Override
 	public float[][] makePixelSamples(Sampler sampler, int n) {
 		return sampler.makeSamples(n, 2);
-	}
-
-	private class RussianRouletteIterator implements Iterator<Float>{
-
-		int i = 0;
-		private float[] floats;
-		private float last;
-		
-		public RussianRouletteIterator(float... floats) {
-			this.floats = floats;
-			this.last = floats[floats.length - 1];
-		}
-		@Override
-		public boolean hasNext() {
-			return true;
-		}
-
-		@Override
-		public Float next() {
-			if (i + 1 < floats.length) {
-				float next = floats[i];
-				i++;
-				return next;
-			} else
-				return last;
-		}
-
-		@Override
-		public void remove() {
-			throw new RuntimeException("Not supported.");
-		}
-		
 	}
 }
