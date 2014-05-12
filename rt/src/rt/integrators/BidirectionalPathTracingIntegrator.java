424ad22d418486212a787ec674d962bec47d63d5
panmari <smmuzi@gmail.com>
2014-05-12 17:58:19 +0200

    a new beginning...

--- rt/src/rt/integrators/BidirectionalPathTracingIntegrator.java
+++ rt/src/rt/integrators/BidirectionalPathTracingIntegrator.java
@@ -42,117 +42,17 @@ public class BidirectionalPathTracingIntegrator implements Integrator {
 		this.lightFilm = new BoxFilterFilm(scene.getFilm().getWidth(), scene.getFilm().getHeight());
 	}
 	
-	
 	@Override
 	public Spectrum integrate(Ray primaryRay) {
-		List<PathNode> lightPath = new ArrayList<>();
-		PathNode lightNode = traceLightRay();
-		lightPath.add(lightNode);
-		for (int t = 1; t < MAX_LIGHT_BOUNCES; t++) {
-			Vector3f lastDir = lightNode.next.w;
-			Tuple3f lastOrigin = lightNode.h.position;
-			Ray r = new Ray(lastOrigin, lastDir, t, true);
-			lightNode = makePathNode(r);
-			//TODO: russian roulette
-			if (lightNode == null) {
-				break;
-			}
-			lightPath.add(lightNode);
+		//make eye path
+		//make first node
+		for (int eyeBounce = 0; eyeBounce < MAX_EYE_BOUNCES; eyeBounce++) {
+			Spectrum alphaE = new Spectrum(1);
 		}
 		
-		Spectrum eyeAlpha = new Spectrum(1);
-		Spectrum outgoing = new Spectrum();
-		Ray r = primaryRay;
-		boolean segmentIsSpecular = false;
-		for (int s = 0; s < MAX_EYE_BOUNCES; s++) {
-			PathNode eye = makePathNode(r);
-			if (eye == null)
-				return outgoing;
-			Spectrum emission = eye.h.material.evaluateEmission(eye.h, eye.h.w);
-			if (emission != null) {
-				if (eye.bounce == 0 || segmentIsSpecular)
-					outgoing.add(emission);
-				break;
-			}
-			Spectrum lightAlpha = new Spectrum(1);
-			for (PathNode light: lightPath) {
-				Spectrum contribution = connect(eye, light);
-				contribution.mult(lightAlpha);
-				contribution.mult(eyeAlpha);
-				if (light.bounce == 0)
-					lightAlpha.mult(light.next.emission);
-				else
-					lightAlpha.mult(light.next.brdf);
-				lightAlpha.mult(light.Gp);
-				contribution.mult(1f/(s+light.bounce+1));
-				outgoing.add(contribution);
-			}
-			segmentIsSpecular = eye.next.isSpecular;
-			r = new Ray(eye.h.position, eye.next.w, r.depth + 1, true);
-			eyeAlpha.mult(eye.next.brdf);
-			eyeAlpha.mult(eye.Gp);
+		for (int lightBounce = 0; lightBounce < MAX_LIGHT_BOUNCES; lightBounce++) {
+			Spectrum alphaL = new Spectrum(1);
 		}
-		return outgoing;
-	}
-	
-	public Spectrum connect(PathNode eye, PathNode light) {
-		// pointing from light to eye spot
-		Vector3f connection = StaticVecmath.sub(eye.h.position, light.h.position);
-		float connection_d2 = connection.lengthSquared();
-		Vector3f normedConnection = new Vector3f();
-		normedConnection.normalize(connection);
-
-		Spectrum s = eye.h.material.evaluateBRDF(eye.h, eye.h.w, StaticVecmath.negate(normedConnection));
-		Spectrum lightPathSpectrum;
-		if (light.bounce == 0) {
-			lightPathSpectrum = light.h.material.evaluateEmission(light.h, normedConnection);
-			lightPathSpectrum.mult(1/light.h.p);
-		} else {
-			lightPathSpectrum = light.h.material.evaluateBRDF(light.h, eye.h.w, normedConnection);
-		}
-		s.mult(lightPathSpectrum);
-
-		float cosLightPath;
-		if (light.h.normal != null) {
-			cosLightPath = light.h.normal.dot(normedConnection);
-			if (cosLightPath <= 0)
-				return new Spectrum(); // stay black if hit light from behind
-		} else cosLightPath = 1; //for point lights
-		float cosEyePath = StaticVecmath.negate(normedConnection).dot(eye.h.normal);
-		float connectionG = cosLightPath*cosEyePath/connection_d2;
-		s.mult(connectionG);
-
-		// shadow ray
-		Ray r = new Ray(light.h.position, connection, 0, true);
-		HitRecord shadowHit = root.intersect(r);
-		if (shadowHit != null && shadowHit.t < 0.99f && shadowHit.material.castsShadows())
-			return new Spectrum();
-		else
-			return s;
-	}
-	
-	public PathNode makePathNode(Ray r) {
-		HitRecord h = root.intersect(r);
-		if (h == null)
-			return null;		
-		float[][] sample = this.sampler.makeSamples(1, 2);
-		ShadingSample next = h.material.getShadingSample(h, sample[0]);
-		float Gp = 1/next.p;
-		if (!next.isSpecular)
-			Gp *= h.normal.dot(next.w);
-		return new PathNode(h, Gp, next, r.depth);
-	}
-	
-	private PathNode traceLightRay() {
-		LightGeometry lightSource = lightList.getRandomLight(this.sampler.makeSamples(1, 2));
-		float[][] sample = this.sampler.makeSamples(1, 2);
-		HitRecord lightHit = lightSource.sample(sample[0]);
-		lightHit.p *= 1f/lightList.size();
-		sample = this.sampler.makeSamples(1, 2);
-		ShadingSample emission = lightHit.material.getEmissionSample(lightHit, sample[0]);
-
-		float Gp = 1/lightHit.p;
-		return new PathNode(lightHit, Gp, emission, 0);
 	}
 
 	@Override
--- rt/src/rt/integrators/PathNode.java
+++ rt/src/rt/integrators/PathNode.java
@@ -2,19 +2,20 @@ package rt.integrators;
 
 import rt.HitRecord;
 import rt.Material.ShadingSample;
+import rt.Spectrum;
 
 public class PathNode {
+	Spectrum alpha;
+	float geometryTerm;
+	float p_L;
+	float p_E;
 	
-	final HitRecord h;
-	final ShadingSample next;
-	float Gp;
-	final int bounce;
-	
-	public PathNode(HitRecord h, float Gp, ShadingSample next, int bounce) {
-		this.h = h;
-		this.Gp = Gp;
-		this.next = next;
-		this.bounce = bounce;
+	public PathNode(Spectrum alpha, float geometryTerm, float p_L, float p_E) {
+		this.alpha = alpha;
+		this.geometryTerm = geometryTerm;
+		this.p_L = p_L;
+		this.p_E = p_E;
 	}
+	
 	
 }
